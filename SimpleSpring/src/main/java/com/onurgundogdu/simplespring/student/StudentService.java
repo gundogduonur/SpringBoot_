84cc72af32ddffd890a91c9fe76bfbd47551437d
Onur Gündoğdu <gundogdusoftware@outlook.com>
2022-01-06 15:52:24 +0300

    using @Post

--- SimpleSpring/src/main/java/com/onurgundogdu/simplespring/student/StudentService.java
+++ SimpleSpring/src/main/java/com/onurgundogdu/simplespring/student/StudentService.java
@@ -3,8 +3,6 @@ package com.onurgundogdu.simplespring.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @Service
@@ -19,5 +17,10 @@ public class StudentService {
     public List<Student> getStudents()
     {
         return studentRepository.findAll();
+    }
+
+    public void addStudent(Student student)
+    {
+     System.out.println(student);
     }
 }
